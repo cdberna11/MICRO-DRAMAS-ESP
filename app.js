e913da5a449833cc60cd295569397dc8e4b7fefd
cdberna11 <christopherbernal11@gmail.com>
2026-08-11 22:46:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+async function cargarDramas() {
+    const respuesta = await fetch("dramas.json");
+    const dramas = await respuesta.json();
 
+    const catalogo = document.getElementById("catalogo");
+
+    catalogo.innerHTML = "";
+
+    dramas.forEach(drama => {
+
+        catalogo.innerHTML += `
+        <div class="card">
+
+            ${drama.portada}="${drama.titulo}">
+
+            <div class="card-content">
+
+                <h2>${drama.titulo}</h2>
+
+                <p>${drama.plataforma}</p>
+
+                <p>${drama.descripcion}</p>
+
+                ${drama.video}
+                    Ver ahora
+                </a>
+
+            </div>
+
+        </div>
+        `;
+    });
+}
+
+cargarDramas();
